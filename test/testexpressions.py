8ed670bdbada1be85c5cecb199f3d37469860bb6
jbytecode <mhsatman@gmail.com>
2019-05-28 15:19:08 +0300

    new test

--- test/testexpressions.py
+++ test/testexpressions.py
@@ -1,54 +1,16 @@
 import unittest
-from gp.interpreter import *
+from interpreter import *
 
 class TestExpressions(unittest.TestCase):
 
-    def test_plus_expression(self):
-        envir = Environment()
-        pe = PlusExpression(
-            [
-                NumericExpression([100]),
-                NumericExpression([960])
-            ]
-        )
-        result = pe.eval(envir)
-        self.assertEqual(1060, result)
-
-
-    def test_minus_expression(self):
-        envir = Environment()
-        pe = MinusExpression(
-            [
-                NumericExpression([100]),
-                NumericExpression([960])
-            ]
-        )
-        result = pe.eval(envir)
-        self.assertEqual(-860, result)
-
-
-    def test_product_expression(self):
-        envir = Environment()
-        pe = ProductExpression(
-            [
-                NumericExpression([100]),
-                NumericExpression([960])
-            ]
-        )
-        result = pe.eval(envir)
-        self.assertEqual(96000, result)
-
-
-    def test_divide_expression(self):
-        envir = Environment()
-        pe = DivideExpression(
-            [
-                NumericExpression([1000]),
-                NumericExpression([100])
-            ]
-        )
-        result = pe.eval(envir)
-        self.assertEqual(10, result)
+    def test_postfix_expression(self):
+        funclist = [
+            PlusFunction("+", 2),
+            MinusFunction("-", 2)
+        ]
+        identifiers = {"x": 3, "y": 10}
+        result = postfixeval([2,2, "+", "y", "-"], funclist, identifiers)
+        self.assertEqual(result[0], 6)
 
 
 
