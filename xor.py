5f6705f937a6ea3f0403f97950c5bd8674168659
jbytecode <mhsatman@gmail.com>
2019-06-12 20:46:32 +0300

    xor added. Gp discovers y = | x2 - x1| for xor data

---
+++ xor.py
@@ -0,0 +1,32 @@
+from gp import *
+
+if __name__ == "__main__":
+    varlist = {"x1": 1, "x2": 1}
+    constantpool = [i  for i in range(1,5)]
+    myfunclist = [
+        PlusFunction("+", 2),
+        MinusFunction("-", 2),
+        ProductFunction("*", 2),
+        DivideFunction("/", 2),
+        AbsFunction("Abs", 1)
+        ]
+
+    def f (code):
+        x1 = [1, 0, 1, 0]
+        x2 = [0, 0, 1, 1]
+        y  = [1, 0, 0, 1]
+        
+        total = 0.0
+        for i in range(len(x1)):
+            resulti = postfixeval(code, myfunclist, {"x1": x1[i], "x2": x2[i]})[0]
+            total = total + math.pow(y[i] - resulti, 2.0)
+        return -total
+
+
+    gp = GP(f, myfunclist ,varlist,constantpool, popsize = 100,  deep = 10, maxdeep = 10)
+    gp.createRandomPopulation()
+    for i in range(1):
+        gp.iterate()
+
+    gp.interactive()
+    
